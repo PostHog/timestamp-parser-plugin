030dbb755b6982e760bff4388c41003c205dde4a
yakkomajuri <yakko.majuri@gmail.com>
2021-01-27 15:46:44 +0000

    Add plugin

---
+++ index.js
@@ -0,0 +1,14 @@
+function processEvent(event) {
+    
+    if (event.properties && event.properties['$time']) {
+        const timestamp = event.properties['$time']
+        event.properties['day_of_the_week'] = new Date(timestamp).toLocaleDateString('en-GB', {weekday: 'long'})
+        const date = new Date(timestamp).toLocaleDateString('en-GB').split('/')
+        event.properties['day'] = date[0]
+        event.properties['month'] = date[1]
+        event.properties['year'] = date[2]
+
+    }
+    
+    return event
+}
