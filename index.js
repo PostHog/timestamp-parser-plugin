2098a4e67bfe06f5b1e3fb2e60637e34351d02dc
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2021-11-09 08:29:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function processEvent(event) {
     if (event.properties && event.properties['$time']) {
+        const eventDate = new Date(timestamp)
         const timestamp = event.properties['$time'] * 1000
-        event.properties['day_of_the_week'] = new Date(timestamp).toLocaleDateString('en-GB', { weekday: 'long' })
-        const date = new Date(timestamp).toLocaleDateString('en-GB').split('/')
+        event.properties['day_of_the_week'] = eventDate.toLocaleDateString('en-GB', { weekday: 'long' })
+        const date = eventDate.toLocaleDateString('en-GB').split('/')
         event.properties['day'] = Number(date[0])
         event.properties['month'] = Number(date[1])
         event.properties['year'] = Number(date[2])
+        event.properties['hour'] = eventDate.getHours()
+        event.properties['minute'] = eventDate.getMinutes()
     }
 
     return event
